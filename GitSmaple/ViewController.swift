c83b03d9c0bd93ce159ed41a2905bbf3faf4b8b3
Sanju <sanju.nandela@gmail.com>
2018-11-21 11:58:54 +0530

    Initial Commit

---
+++ GitSmaple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitSmaple
+//
+//  Created by JMRIMAC-1 on 11/21/18.
+//  Copyright © 2018 JMRIMAC-1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
